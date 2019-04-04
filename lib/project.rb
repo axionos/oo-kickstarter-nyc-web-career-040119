2a2e980f3b4812334b2deff9f95ed85440db173c
github username <github email address>
2019-04-04 10:37:32 -0400

    submitting the lab @eddgr

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Project
+  attr_reader :title
+  attr_accessor :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer_instance)
+    @backers << backer_instance
+    backer_instance.backed_projects << self
+    # backer_instance.back_project(self)
+  end
+end
+
+# edgar.backed_projects # => []
