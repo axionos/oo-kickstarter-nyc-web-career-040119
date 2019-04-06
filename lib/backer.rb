29570fbf5374d024e09b8f9069ede466d1cbadec
github username <github email address>
2019-04-06 12:22:47 -0400

    refactored code after submission

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,24 +1,39 @@
-
 class Backer
-  attr_reader :name
-  attr_accessor :backed_projects
+  attr_reader :name, :backed_projects
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
 
-  def back_project(project_instance)
-    @backed_projects << project_instance
-    project_instance.add_backer(self)
-    # project_instance.backers << self #more direct
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
   end
 end
 
-# #separate
-# water_bottle = Project.new("Water Bottle")
-# #separate
-# edgar = Backer.new("Edgar")
+
 #
-# #connect
-# edgar.back_project(water_bottle)
+# class Backer
+#   attr_reader :name
+#   attr_accessor :backed_projects
+#
+#   def initialize(name)
+#     @name = name
+#     @backed_projects = []
+#   end
+#
+#   def back_project(project_instance)
+#     @backed_projects << project_instance
+#     project_instance.add_backer(self)
+#     # project_instance.backers << self #more direct
+#   end
+# end
+#
+# # #separate
+# # water_bottle = Project.new("Water Bottle")
+# # #separate
+# # edgar = Backer.new("Edgar")
+# #
+# # #connect
+# # edgar.back_project(water_bottle)
--- lib/project.rb
+++ lib/project.rb
@@ -1,19 +1,37 @@
 require 'pry'
 
 class Project
-  attr_reader :title
-  attr_accessor :backers
+  attr_reader :title, :backers
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-  def add_backer(backer_instance)
-    @backers << backer_instance
-    backer_instance.backed_projects << self
-    # backer_instance.back_project(self)
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
   end
 end
 
-# edgar.backed_projects # => []
+
+
+
+
+# class Project
+#   attr_reader :title
+#   attr_accessor :backers
+#
+#   def initialize(title)
+#     @title = title
+#     @backers = []
+#   end
+#
+#   def add_backer(backer_instance)
+#     @backers << backer_instance
+#     backer_instance.backed_projects << self
+#     # backer_instance.back_project(self)
+#   end
+# end
+#
+# # edgar.backed_projects # => []
